d4447fecb1005a770bd208f03825e07d54264b61
Kunal Vats <1kunalvats9@gmail.com>
2025-04-06 15:36:08 +0530

    done

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,16 @@
 import express from "express";
 import User from "../models/userModel.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.log("DB Error:", err));
 
 // POST /api/user/get
 router.post("/get", async (req, res) => {
